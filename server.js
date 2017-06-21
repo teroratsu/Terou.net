5a708ea5f3355b994644c3e2e50ca83cbcc805dd
Tero <contact.tero@gmail.com>
2017-06-21 18:14:53 +0200

    -FIX bad typo

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ function ctrlArt(req, res) {
 /**************************************/
 /************** SERVER ****************/
 
-server.listen(server_port, server_ip_address, function(){
+http.listen(server_port, server_ip_address, function(){
   console.log("Listening on " + server_ip_address + ", server_port " + server_port)
 });
 
